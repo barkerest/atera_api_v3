dfcd7f9d98b49229722bf014ef7287dbe6a64fd9
Beau Barker <bbarker@ckindustrial.com>
2020-01-23 16:27:27 -0500

    Fix minor bug in Custom Values, endpoint returns array not single object.

--- AteraAPI.V3/Models/Internal/CustomValueApiEndpoint.cs
+++ AteraAPI.V3/Models/Internal/CustomValueApiEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using AteraAPI.V3.Enums;
@@ -20,12 +21,12 @@ namespace AteraAPI.V3.Models.Internal
 		public ICustomValue Find(string id) => FindAsync(id).Result;
 
 		public async Task<ICustomValue> FindAsync(string id)
-			=> await _context.ExecuteAsync<CustomValue>($"{_name}/{HttpUtility.UrlEncode(id)}");
+			=> (await _context.ExecuteAsync<CustomValue[]>($"{_name}/{HttpUtility.UrlEncode(id)}")).FirstOrDefault();
 
 		public ICustomValue FindFor(CustomFieldTarget targetType, int targetId, string fieldName)
 			=> FindForAsync(targetType, targetId, fieldName).Result;
 
 		public async Task<ICustomValue> FindForAsync(CustomFieldTarget targetType, int targetId, string fieldName)
-			=> await _context.ExecuteAsync<CustomValue>($"{_name}/{targetType.ToString().ToLower()}field/{targetId}/{HttpUtility.UrlEncode(fieldName)}");
+			=> (await _context.ExecuteAsync<CustomValue[]>($"{_name}/{targetType.ToString().ToLower()}field/{targetId}/{HttpUtility.UrlEncode(fieldName)}")).FirstOrDefault();
 	}
 }
